58aee46abbe274b98040e2e4e902acb06da245b4
Phlam Sicusa <sicusa@gilatod.art>
2022-12-24 12:37:45 +0800

    fix OpenGLSynchronizer

--- Nagule.Graphics.Backend.OpenTK/Layers/Rendering/OpenGLSynchronizer.cs
+++ Nagule.Graphics.Backend.OpenTK/Layers/Rendering/OpenGLSynchronizer.cs
@@ -5,13 +5,24 @@ using global::OpenTK.Graphics.OpenGL;
 
 using Aeco;
 
-public class OpenGLSynchronizer : VirtualLayer, ILoadListener, IRenderListener, IRenderFinishedListener
+public class OpenGLSynchronizer : VirtualLayer, ILoadListener, IUnloadListener, ILateUpdateListener, IRenderListener, IRenderFinishedListener
 {
     private GLSync _sync;
+    private AutoResetEvent _renderFinishedEvent = new(true);
 
     public void OnLoad(IContext context)
     {
         _sync = GL.FenceSync(SyncCondition.SyncGpuCommandsComplete, SyncBehaviorFlags.None);
+    }
+
+    public void OnUnload(IContext context)
+    {
+        _renderFinishedEvent.Set();
+    }
+
+    public void OnLateUpdate(IContext context, float deltaTime)
+    {
+        _renderFinishedEvent.WaitOne();
     }
 
     public void OnRender(IContext context, float deltaTime)
@@ -26,5 +37,6 @@ public class OpenGLSynchronizer : VirtualLayer, ILoadListener, IRenderListener, 
     {
         GL.DeleteSync(_sync);
         _sync = GL.FenceSync(SyncCondition.SyncGpuCommandsComplete, SyncBehaviorFlags.None);
+        _renderFinishedEvent.Set();
     }
 }
