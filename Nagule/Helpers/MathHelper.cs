16a7b309cda87b97d57decd2bfdce5ada5a08ec0
Phlam Sicusa <sicusa@gilatod.art>
2023-01-06 21:44:07 +0800

    add math helper

--- Nagule/Helpers/MathHelper.cs
+++ Nagule/Helpers/MathHelper.cs
@@ -2,7 +2,7 @@ namespace Nagule;
 
 using System.Numerics;
 
-public static class MathExtensions
+public static class MathHelper
 {
     public const float TwoPI = MathF.PI * 2;
     public const float DegreeToRadian = TwoPI / 360;
@@ -26,6 +26,61 @@ public static class MathExtensions
         angles.Z = MathF.Atan2(sinyCosp, cosyCosp);
 
         return angles * RadianToDegree;
+    }
+
+    // from https://answers.unity.com/questions/467614/what-is-the-source-code-of-quaternionlookrotation.html
+    public static Quaternion LookRotation(Vector3 forward, Vector3 up)
+    {
+        forward = Vector3.Normalize(forward);
+        Vector3 right = Vector3.Normalize(Vector3.Cross(forward, up));
+        up = Vector3.Cross(right, forward);
+        
+        var m00 = right.X;
+        var m01 = right.Y;
+        var m02 = right.Z;
+        var m10 = up.X;
+        var m11 = up.Y;
+        var m12 = up.Z;
+        var m20 = forward.X;
+        var m21 = forward.Y;
+        var m22 = forward.Z;
+    
+        float num8 = (m00 + m11) + m22;
+        var quaternion = new Quaternion();
+        if (num8 > 0f) {
+            var num = (float)Math.Sqrt(num8 + 1f);
+            quaternion.W = num * 0.5f;
+            num = 0.5f / num;
+            quaternion.X = (m12 - m21) * num;
+            quaternion.Y = (m20 - m02) * num;
+            quaternion.Z = (m01 - m10) * num;
+            return quaternion;
+        }
+        if ((m00 >= m11) && (m00 >= m22)) {
+            var num7 = (float)Math.Sqrt(((1f + m00) - m11) - m22);
+            var num4 = 0.5f / num7;
+            quaternion.X = 0.5f * num7;
+            quaternion.Y = (m01 + m10) * num4;
+            quaternion.Z = (m02 + m20) * num4;
+            quaternion.W = (m12 - m21) * num4;
+            return quaternion;
+        }
+        if (m11 > m22) {
+            var num6 = (float)Math.Sqrt(((1f + m11) - m00) - m22);
+            var num3 = 0.5f / num6;
+            quaternion.X = (m10+ m01) * num3;
+            quaternion.Y = 0.5f * num6;
+            quaternion.Z = (m21 + m12) * num3;
+            quaternion.W = (m20 - m02) * num3;
+            return quaternion; 
+        }
+        var num5 = (float)Math.Sqrt(((1f + m22) - m00) - m11);
+        var num2 = 0.5f / num5;
+        quaternion.X = (m20 + m02) * num2;
+        quaternion.Y = (m21 + m12) * num2;
+        quaternion.Z = 0.5f * num5;
+        quaternion.W = (m01 - m10) * num2;
+        return quaternion;
     }
 
     public static Quaternion ToQuaternion(this Vector3 v)
