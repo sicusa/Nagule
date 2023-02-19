d8387b01d80341b98ad8dd3bddb1f39f28ea5d53
Phlam Sicusa <sicusa@gilatod.art>
2023-02-19 18:15:23 +0800

    add generic version of TypedKey

--- Nagule/Data/TypedKey.cs
+++ Nagule/Data/TypedKey.cs
@@ -1,5 +1,10 @@
 namespace Nagule;
 
+public record struct TypedKey<TKey, TValue>(TKey Key)
+{
+    public static implicit operator TypedKey<TKey, TValue>(TKey key) => new(key);
+}
+
 public record struct TypedKey<TValue>(string Name)
 {
     public static implicit operator TypedKey<TValue>(string name) => new(name);
