923d78b73e5287bc489992196f5e7fad13e79428
Phlam Sicusa <sicusa@gilatod.art>
2023-03-11 23:33:46 +0800

    small refinements

--- Nagule.Graphics.Backend.OpenTK/Pipelines/CompositionPipeline/Passes/GammaCorrectionPass.cs
+++ Nagule.Graphics.Backend.OpenTK/Pipelines/CompositionPipeline/Passes/GammaCorrectionPass.cs
@@ -4,14 +4,14 @@ public class GammaCorrectionPass : CompositionPassBase
 {
     public override IEnumerable<MaterialProperty> Properties { get; } 
 
+    public override string EntryPoint { get; } = "GammaCorrection";
+    public override string Source { get; }
+        = GraphicsHelper.LoadEmbededShader("nagule.pipeline.gamma_correction.comp.glsl");
+
     public GammaCorrectionPass(float gamma = 2.2f)
     {
         Properties = new MaterialProperty[] {
             new("GammaCorrection_Gamma", gamma)
         };
     }
-
-    public override string EntryPoint { get; } = "GammaCorrection";
-    public override string Source { get; }
-        = GraphicsHelper.LoadEmbededShader("nagule.pipeline.gamma_correction.comp.glsl");
 }
